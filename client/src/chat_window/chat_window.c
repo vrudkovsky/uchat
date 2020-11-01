5c41bf9b872c83ffc8d522544cf02f1f9b98515a
mdudnyk <mdudnyk.sps@gmail.com>
2020-11-01 18:24:02 +0200

    css connected

--- client/src/chat_window/chat_window.c
+++ client/src/chat_window/chat_window.c
@@ -10,6 +10,11 @@ void chat_window(void) {
     chat.contacts_list = GTK_WIDGET(gtk_builder_get_object(chat.builder, "contacts_list"));
     chat.chats_list = GTK_WIDGET(gtk_builder_get_object(chat.builder, "chats_list"));
 
+    GdkScreen * display = gdk_screen_get_default();
+    GtkCssProvider *css = gtk_css_provider_new();
+	gtk_css_provider_load_from_path(css, "glade/theme.css", NULL);
+    gtk_style_context_add_provider_for_screen(display, GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    
     g_signal_connect(chat.ch_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_builder_connect_signals(chat.builder, NULL);
 
